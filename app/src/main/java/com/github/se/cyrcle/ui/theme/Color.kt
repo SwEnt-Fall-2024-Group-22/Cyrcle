b53b4b64072de657210b15d7099094c257d3a148
LouisVasseur <louisvasseur996@gmail.com>
2024-10-04 14:12:51 +0200

    feat: provide style code backbone
    add Color, Theme, and Type to define CyrcleTheme

---
+++ app/src/main/java/com/github/se/cyrcle/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.github.se.cyrcle.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val Cerulean = Color(0xFF22799B)
+val LightBlue = Color(0xFFD1E7F8)
+val Cyan = Color(0xFF80DEF3)
+val NormalBlue = Color(0xFF00D0FF)
+val DarkBlue = Color(0xFF00A1FF)
