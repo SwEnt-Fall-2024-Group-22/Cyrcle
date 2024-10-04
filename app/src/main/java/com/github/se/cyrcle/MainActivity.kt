c4bf522dbde81b6121a04140b7516a55ef95624e
Mattéo Santini <mattsantini2003@gmail.com>
2024-10-04 17:50:50 +0200

    feature: add sign in screen
    Add main composable and integrated it to the main screen.
    Need to:
    * Incorporate navigation once it is there
    * Add tests
    * Replace deprecated sign in API with Credential manager

--- app/src/main/java/com/github/se/cyrcle/MainActivity.kt
+++ app/src/main/java/com/github/se/cyrcle/MainActivity.kt
@@ -3,11 +3,34 @@ package com.github.se.cyrcle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.github.se.cyrcle.ui.authentication.SignInScreen
+import com.github.se.cyrcle.ui.theme.CyrcleTheme
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : ComponentActivity() {
+  private lateinit var auth: FirebaseAuth
+
+
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
-    setContent { }
+    // Initialize Firebase Auth
+    auth = FirebaseAuth.getInstance()
+    auth.currentUser?.let {
+      // Sign out the user if they are already signed in
+      auth.signOut()
+    }
+
+    setContent {
+      CyrcleTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+          SignInScreen()
+        }
+      }
+    }
   }
 }
