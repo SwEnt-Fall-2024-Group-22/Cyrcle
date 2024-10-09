c7fcc3d718a444cbc49b46693c73dbaffcd05400
Perazzolo Sean <sean.perazzolo@gmail.com>
2024-10-09 20:52:51 +0200

    feat: add more tests for getParkingsByLocation()

--- app/src/androidTest/java/com/github/se/cyrcle/model/parking/ParkingViewModelTest2.kt
+++ app/src/androidTest/java/com/github/se/cyrcle/model/parking/ParkingViewModelTest2.kt
@@ -25,17 +25,17 @@ class ParkingViewModelTest2 {
     parkingRepositoryFirestore = ParkingRepositoryFirestore(FirebaseFirestore.getInstance())
     imageRepository = mock(ImageRepository::class.java)
     parkingViewModel = ParkingViewModel(imageRepository, parkingRepositoryFirestore)
+
+    parkingViewModel.addParking(TestInstancesParking.parking1)
+    parkingViewModel.addParking(TestInstancesParking.parking2)
+    parkingViewModel.addParking(TestInstancesParking.parking3)
+
+    // FIXME should wait for return
+    Thread.sleep(5000)
   }
 
   @Test
   fun getParkingUidTest() {
-    // Add parkings to the database
-    parkingViewModel.addParking(TestInstancesParking.parking1)
-    parkingViewModel.addParking(TestInstancesParking.parking2)
-    parkingViewModel.addParking(TestInstancesParking.parking3)
-    // FIXME should wait for return
-    Thread.sleep(5000)
-
     // Get the parking with the uid
     parkingViewModel.getParking(TestInstancesParking.parking1.uid)
     Thread.sleep(5000)
@@ -47,13 +47,6 @@ class ParkingViewModelTest2 {
 
   @Test
   fun getParkingsBetweenTest() {
-    // Add parkings to the database
-    parkingViewModel.addParking(TestInstancesParking.parking1)
-    parkingViewModel.addParking(TestInstancesParking.parking2)
-    parkingViewModel.addParking(TestInstancesParking.parking3)
-    // FIXME should wait for return
-    Thread.sleep(5000)
-
     // Get parkings between the two points
     parkingViewModel.getParkings(Point(46.0, 6.0), Point(47.0, 7.0))
     Thread.sleep(5000)
@@ -67,19 +60,47 @@ class ParkingViewModelTest2 {
     assert(parkingsReturned.contains(TestInstancesParking.parking2))
   }
 
-  // FIXME this function needs more testing
   @Test
-  fun getParkingLocation() {
-    // Add parkings to the database
-    parkingViewModel.addParking(TestInstancesParking.parking1)
-    parkingViewModel.addParking(TestInstancesParking.parking2)
-    parkingViewModel.addParking(TestInstancesParking.parking3)
-    // FIXME should wait for return
+  fun getParkingLocationKIs0() {
+    // Get the two closest parkings to the location
+    parkingViewModel.getParkingsByLocation(Point(47.1, 7.1), 0)
     Thread.sleep(5000)
 
+    // Check if the parkings returned are the correct ones
+    val parkingsReturned = parkingViewModel.displayedSpots.value
+    assert(parkingsReturned.isEmpty())
+  }
+
+  @Test
+  fun getParkingLocationKIs1() {
     // Get the two closest parkings to the location
     parkingViewModel.getParkingsByLocation(Point(47.1, 7.1), 1)
     Thread.sleep(5000)
+
+    // Check if the parkings returned are the correct ones
+    val parkingsReturned = parkingViewModel.displayedSpots.value
+    assert(parkingsReturned.size == 1)
+    assert(parkingsReturned[0] == TestInstancesParking.parking3)
+  }
+
+  @Test
+  fun getParkingLocationKIs2() {
+    // Get the two closest parkings to the location
+    parkingViewModel.getParkingsByLocation(Point(46.69, 6.9), 2)
+    Thread.sleep(10000)
+
+    // Check if the parkings returned are the correct ones
+    val parkingsReturned = parkingViewModel.displayedSpots.value
+    assert(parkingsReturned.size == 2)
+    assert(parkingsReturned[0] == TestInstancesParking.parking2)
+    assert(parkingsReturned[1] == TestInstancesParking.parking3)
+  }
+
+  @Test
+  fun getParkingLocationKIs2Only1PointInRange() {
+    // Get the two closest parkings to the location
+    parkingViewModel.getParkingsByLocation(Point(46.9, 6.7), 2)
+    Thread.sleep(10000)
 
     // Check if the parkings returned are the correct ones
     val parkingsReturned = parkingViewModel.displayedSpots.value
