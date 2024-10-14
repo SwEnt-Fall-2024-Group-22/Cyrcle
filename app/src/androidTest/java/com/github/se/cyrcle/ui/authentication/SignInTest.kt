4e92eb59e91b703939a692c3b90732337d5e4897
Mattéo Santini <137495412+matttsss@users.noreply.github.com>
2024-10-14 11:57:35 +0200

    fix: intermittent Authentication tests error (#38)
    Set composable manualy and remove unused inheritance

--- app/src/androidTest/java/com/github/se/cyrcle/ui/authentication/SignInTest.kt
+++ app/src/androidTest/java/com/github/se/cyrcle/ui/authentication/SignInTest.kt
@@ -1,32 +1,41 @@
 package com.github.se.cyrcle.ui.authentication
 
+import androidx.activity.compose.setContent
 import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
+import androidx.navigation.NavHostController
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.se.cyrcle.MainActivity
-import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import com.github.se.cyrcle.ui.navigation.NavigationActions
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
 
 @RunWith(AndroidJUnit4::class)
-class LoginTest : TestCase() {
+class LoginTest {
   @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+  private lateinit var navigationHost: NavHostController
+  private lateinit var navigationActions: NavigationActions
 
   // The IntentsTestRule is not reliable.
 
   @Before
   fun setUp() {
     Intents.init()
+
+    navigationHost = mock(NavHostController::class.java)
+    navigationActions = NavigationActions(navigationHost)
   }
 
   // Release Intents after each test
@@ -37,6 +46,7 @@ class LoginTest : TestCase() {
 
   @Test
   fun titleAndButtonAreCorrectlyDisplayed() {
+    composeTestRule.activity.setContent { SignInScreen(navigationActions) }
     composeTestRule.onNodeWithTag("loginTitle").assertIsDisplayed()
     composeTestRule.onNodeWithTag("loginTitle").assertTextEquals("Welcome to Cyrcle")
 
@@ -46,6 +56,8 @@ class LoginTest : TestCase() {
 
   @Test
   fun googleSignInReturnsValidActivityResult() {
+    composeTestRule.activity.setContent { SignInScreen(navigationActions) }
+
     composeTestRule.onNodeWithTag("loginButton").performClick()
     composeTestRule.waitForIdle()
     // assert that an Intent resolving to Google Mobile Services has been sent (for sign-in)
