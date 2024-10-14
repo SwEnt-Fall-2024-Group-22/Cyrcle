702a2d0de60863c8f684c1623a2773ad3089634a
Perazzolo Sean <sean.perazzolo@gmail.com>
2024-10-14 11:48:42 +0200

    fix: use our colors for the theme instead of default one

--- app/src/main/java/com/github/se/cyrcle/ui/theme/Theme.kt
+++ app/src/main/java/com/github/se/cyrcle/ui/theme/Theme.kt
@@ -1,20 +1,16 @@
 package com.github.se.cyrcle.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme =
     darkColorScheme(primary = Cerulean, secondary = White, tertiary = Black)
 
 private val LightColorScheme =
-    lightColorScheme(primary = DarkBlue, secondary = LightBlue, tertiary = Black)
+    lightColorScheme(primary = Cerulean, secondary = LightBlue, tertiary = Black)
 
 /* Other default colors to override if/when needed
 background = Color(0xFFFFFBFE),
@@ -27,20 +23,12 @@ onSurface = Color(0xFF1C1B1F),
 */
 
 @Composable
-fun CyrcleTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
-) {
+fun CyrcleTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
   val colorScheme =
-      when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-          val context = LocalContext.current
-          if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+      if (darkTheme) {
+        DarkColorScheme
+      } else {
+        LightColorScheme
       }
-
   MaterialTheme(colorScheme = colorScheme, typography = Typography, content = content)
 }
