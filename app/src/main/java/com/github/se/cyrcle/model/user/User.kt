7777d82dd574e850dfe210c1277ae3c8ec1d9ccc
AlexandreBeeDeeBop <alexandre.majchrzak@epfl.ch>
2024-10-17 17:11:59 +0200

    Fix ktfmtFormat issue for pull request.

--- app/src/main/java/com/github/se/cyrcle/model/user/User.kt
+++ app/src/main/java/com/github/se/cyrcle/model/user/User.kt
@@ -1,7 +1,5 @@
 package com.github.se.cyrcle.model.user
 
-import com.github.se.cyrcle.model.parking.Location
-import com.google.firebase.Timestamp
 data class User(
     val userId: String = "",
     val username: String = "",
@@ -11,11 +9,11 @@ data class User(
     val profilePictureUrl: String? = null,
     val homeAddress: String? = null,
     val favoriteParkingSpots: List<String> = emptyList(),
-    //TODO For some reason these break my serialization/
+    // TODO For some reason these break my serialization/
     // deserialization and i dont have the time to fix it :
-    //val lastKnownLocation: Location? = null,
-    //val lastLoginTime: Timestamp? = null,
-    //val accountCreationDate: Timestamp? = null,
+    // val lastKnownLocation: Location? = null,
+    // val lastLoginTime: Timestamp? = null,
+    // val accountCreationDate: Timestamp? = null,
     val numberOfContributedSpots: Int = 0,
     val userReputationScore: Int = 0
 )
