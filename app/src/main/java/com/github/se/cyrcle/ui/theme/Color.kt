5dab850d2d8ce29cc3866c77d4812ec6311d91fd
André Peiry <andre.peiry@epfl.ch>
2024-10-18 09:48:59 +0200

    fix: revert theme color

--- app/src/main/java/com/github/se/cyrcle/ui/theme/Color.kt
+++ app/src/main/java/com/github/se/cyrcle/ui/theme/Color.kt
@@ -6,17 +6,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-// val Black = Color(0xFF000000)
-// val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
 val Cerulean = Color(0xFF22799B)
 val LightBlue = Color(0xFFD1E7F8)
-// val Cyan = Color(0xFF80DEF3)
-// val NormalBlue = Color(0xFF00D0FF)
-val DarkBlue = Color(0xFF00A1FF)
-val Madder = Color(0xFF9B2228)
-val GoldenBrown = Color(0xFF9B6C22)
-val GrayLight = Color(0xFF9D9D9D)
-val GrayDark = Color(0xFF555555)
 
 /** A class containing the choice of color for an element. */
 enum class ColorLevel {
@@ -24,38 +17,6 @@ enum class ColorLevel {
   SECONDARY,
   TERTIARY,
   ERROR
-}
-
-/**
- * Generate, based on https://developer.android.com/codelabs/jetpack-compose-theming#3, four
- * versions of the given accent color.
- *
- * @param accentColor The color from which the "palette" should be created
- */
-class AccentColor(accentColor: Color) {
-  val baseColor = accentColor.copy(alpha = 0.40f)
-  val onColor = accentColor.copy(alpha = 1.00f)
-  val containerColor = accentColor.copy(alpha = 0.90f)
-  val onContainerColor = accentColor.copy(alpha = 0.10f)
-}
-
-/**
- * Generate, based on https://developer.android.com/codelabs/jetpack-compose-theming#3, different
- * versions of two neutral colors.
- *
- * @param neutralColor The color from which the "palette" should be created
- * @param neutralVariantColor A variant of the first.
- */
-class NeutralColor(neutralColor: Color, neutralVariantColor: Color) {
-  val backgroundColor = neutralColor.copy(alpha = 0.99f)
-  val onBackgroundColor = neutralColor.copy(alpha = 0.10f)
-  val surfaceColor = neutralColor.copy(alpha = 0.99f)
-  val onSurfaceColor = neutralColor.copy(alpha = 0.10f)
-
-  val surfaceVariantColor = neutralVariantColor.copy(alpha = 0.90f)
-  val onSurfaceVariantColor = neutralVariantColor.copy(alpha = 0.30f)
-  val outlineColor = neutralVariantColor.copy(alpha = 0.50f)
-  val outlineVariantColor = neutralVariantColor.copy(alpha = 0.50f)
 }
 
 /**
--- app/src/main/java/com/github/se/cyrcle/ui/theme/Theme.kt
+++ app/src/main/java/com/github/se/cyrcle/ui/theme/Theme.kt
@@ -5,107 +5,30 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-
-private val primaryColorLight = AccentColor(Cerulean)
-private val secondaryColorLight = AccentColor(Madder)
-private val tertiaryColorLight = AccentColor(GoldenBrown)
-private val errorColorLight = AccentColor(Color.Red)
-private val neutralColorsLight = NeutralColor(GrayLight, GrayDark)
-
-private val primaryColorDark = AccentColor(Cerulean)
-private val secondaryColorDark = AccentColor(Madder)
-private val tertiaryColorDark = AccentColor(GoldenBrown)
-private val errorColorDark = AccentColor(Color.Red)
-private val neutralColorsDark = NeutralColor(GrayDark, GrayLight)
 
 private val DarkColorScheme =
-    darkColorScheme(
-        primary = primaryColorLight.baseColor,
-        onPrimary = primaryColorLight.onColor,
-        primaryContainer = primaryColorLight.containerColor,
-        onPrimaryContainer = primaryColorLight.onContainerColor,
-        secondary = secondaryColorLight.baseColor,
-        onSecondary = secondaryColorLight.onColor,
-        secondaryContainer = secondaryColorLight.containerColor,
-        onSecondaryContainer = secondaryColorLight.onContainerColor,
-        tertiary = tertiaryColorLight.baseColor,
-        onTertiary = tertiaryColorLight.onColor,
-        tertiaryContainer = tertiaryColorLight.containerColor,
-        onTertiaryContainer = tertiaryColorLight.onContainerColor,
-        error = errorColorLight.baseColor,
-        onError = errorColorLight.onColor,
-        errorContainer = errorColorLight.containerColor,
-        onErrorContainer = errorColorLight.onContainerColor,
-        background = neutralColorsLight.backgroundColor,
-        onBackground = neutralColorsLight.onBackgroundColor,
-        surface = neutralColorsLight.surfaceColor,
-        onSurface = neutralColorsLight.onSurfaceColor,
-        surfaceVariant = neutralColorsLight.surfaceVariantColor,
-        onSurfaceVariant = neutralColorsLight.onSurfaceVariantColor,
-        // surfaceDim = ,
-        // surfaceBright = ,
-        // surfaceTint = ,
-        outline = neutralColorsLight.outlineColor,
-        outlineVariant = neutralColorsLight.outlineVariantColor,
-        // inversePrimary = ,
-        // inverseSurface = ,
-        // inverseOnSurface = ,
-        // surfaceContainer = ,
-        // surfaceContainerLow = ,
-        // surfaceContainerLowest = ,
-        // surfaceContainerHigh = ,
-        // surfaceContainerHighest = ,
-        // scrim =
-    )
+    darkColorScheme(primary = Cerulean, secondary = White, tertiary = Black)
 
 private val LightColorScheme =
-    lightColorScheme(
-        primary = primaryColorDark.baseColor,
-        onPrimary = primaryColorDark.onColor,
-        primaryContainer = primaryColorDark.containerColor,
-        onPrimaryContainer = primaryColorDark.onContainerColor,
-        secondary = secondaryColorDark.baseColor,
-        onSecondary = secondaryColorDark.onColor,
-        secondaryContainer = secondaryColorDark.containerColor,
-        onSecondaryContainer = secondaryColorDark.onContainerColor,
-        tertiary = tertiaryColorDark.baseColor,
-        onTertiary = tertiaryColorDark.onColor,
-        tertiaryContainer = tertiaryColorDark.containerColor,
-        onTertiaryContainer = tertiaryColorDark.onContainerColor,
-        error = errorColorDark.baseColor,
-        onError = errorColorDark.onColor,
-        errorContainer = errorColorDark.containerColor,
-        onErrorContainer = errorColorDark.onContainerColor,
-        background = neutralColorsDark.backgroundColor,
-        onBackground = neutralColorsDark.onBackgroundColor,
-        surface = neutralColorsDark.surfaceColor,
-        onSurface = neutralColorsDark.onSurfaceColor,
-        surfaceVariant = neutralColorsDark.surfaceVariantColor,
-        onSurfaceVariant = neutralColorsDark.onSurfaceVariantColor,
-        // surfaceDim = ,
-        // surfaceBright = ,
-        // surfaceTint = ,
-        outline = neutralColorsDark.outlineColor,
-        outlineVariant = neutralColorsDark.outlineVariantColor,
-        // inversePrimary = colorInverse(primaryColorDark.baseColor),
-        // inverseSurface = colorInverse(neutralColorsDark.surfaceColor),
-        // inverseOnSurface = colorInverse(neutralColorsDark.onSurfaceColor),
-        // surfaceContainer = ,
-        // surfaceContainerLow = ,
-        // surfaceContainerLowest = ,
-        // surfaceContainerHigh = ,
-        // surfaceContainerHighest = ,
-        // scrim =
-    )
+    lightColorScheme(primary = Cerulean, secondary = LightBlue, tertiary = Black)
+
+/* Other default colors to override if/when needed
+background = Color(0xFFFFFBFE),
+surface = Color(0xFFFFFBFE),
+onPrimary = Color.White,
+onSecondary = Color.White,
+onTertiary = Color.White,
+onBackground = Color(0xFF1C1B1F),
+onSurface = Color(0xFF1C1B1F),
+*/
 
 @Composable
 fun CyrcleTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-  val colorScheme =
-      if (darkTheme) {
-        DarkColorScheme
-      } else {
-        LightColorScheme
-      }
-  MaterialTheme(colorScheme = colorScheme, typography = Typography, content = content)
+    val colorScheme =
+        if (darkTheme) {
+            DarkColorScheme
+        } else {
+            LightColorScheme
+        }
+    MaterialTheme(colorScheme = colorScheme, typography = Typography, content = content)
 }
