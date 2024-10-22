d59ba92d8b118de8e2ba08dd9b844d1f89b94643
Maria Bennani <maria.bennani@epfl.ch>
2024-10-22 12:33:51 +0200

    refactor: rework user backend

--- app/src/main/java/com/github/se/cyrcle/model/user/User.kt
+++ app/src/main/java/com/github/se/cyrcle/model/user/User.kt
@@ -1,19 +1,26 @@
 package com.github.se.cyrcle.model.user
 
+/**
+ * Data class representing a user.
+ *
+ * @property userId Unique identifier of the user.
+ * @property username Username of the user.
+ * @property firstName First name of the user.
+ * @property lastName Last name of the user.
+ * @property email Email of the user.
+ * @property profilePictureUrl URL of the profile picture of the user.
+ * @property favoriteParkings List of unique identifiers of the user's favorite parkings.
+ */
 data class User(
     val userId: String,
-    val username: String = "",
-    val firstName: String = "",
-    val lastName: String = "",
-    val email: String = "",
-    val profilePictureUrl: String = "",
-    val homeAddress: String = "",
-    val favoriteParkingSpots: List<String> = emptyList(),
-    // TODO For some reason these break my serialization/deserialization and I dont have the time to
-    // fix it :
-    // val lastKnownLocation: Location? = null,
+    val username: String,
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    val profilePictureUrl: String,
+    val favoriteParkings: List<String>,
     // val lastLoginTime: Timestamp? = null,
     // val accountCreationDate: Timestamp? = null,
-    val numberOfContributedSpots: Int = 0,
-    val userReputationScore: Int = 0
+    // val numberOfContributedSpots: Int = 0,
+    // val userReputationScore: Int = 0
 )
--- app/src/main/java/com/github/se/cyrcle/model/user/UserViewModel.kt
+++ app/src/main/java/com/github/se/cyrcle/model/user/UserViewModel.kt
@@ -1,121 +1,114 @@
+package com.github.se.cyrcle.model.user
+
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.github.se.cyrcle.model.user.User
-import com.github.se.cyrcle.model.user.UserRepository
-import com.github.se.cyrcle.model.user.UserRepositoryFirestore
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-class UserViewModel(val userRepository: UserRepository) : ViewModel() {
+class UserViewModel(private val userRepository: UserRepository) : ViewModel() {
 
-  private val _user = MutableStateFlow<User?>(null)
-  val user: StateFlow<User?> = _user
+  private val _currentUser = MutableStateFlow<User?>(null)
+  val currentUser: StateFlow<User?> = _currentUser
 
-  fun fetchUserById(userId: String) {
+  /**
+   * Sets the current user.
+   *
+   * @param user the user to set as the current user
+   */
+  fun setCurrentUser(user: User) {
+    _currentUser.value = user
+  }
+
+  /**
+   * Gets a user by ID from the Firestore database and sets it as the current user.
+   *
+   * @param userId the ID of the user to get
+   * @return the user with the given ID
+   */
+  fun getUserById(userId: String) {
     userRepository.getUserById(
         userId,
-        onSuccess = { fetchedUser -> _user.value = fetchedUser },
+        onSuccess = { setCurrentUser(it) },
         onFailure = { exception ->
-          Log.e("UserViewModel", "Failed to fetch user by ID: $userId", exception)
+          Log.e(
+              "com.github.se.cyrcle.model.user.UserViewModel",
+              "Failed to fetch user by ID: $userId",
+              exception)
         })
   }
 
+  /**
+   * Adds a user to the Firestore database.
+   *
+   * @param user the user to add
+   */
   fun addUser(user: User) {
     userRepository.addUser(
         user,
         onSuccess = {},
         onFailure = { exception ->
-          Log.e("UserViewModel", "Failed to add user: ${user.userId}", exception)
+          Log.e(
+              "com.github.se.cyrcle.model.user.UserViewModel",
+              "Failed to add user: ${user.userId}",
+              exception)
         })
   }
 
+  /**
+   * Updates a user in the Firestore database.
+   *
+   * @param user the user to update
+   */
   fun updateUser(user: User) {
     userRepository.updateUser(
         user,
-        onSuccess = {
-          // Handle success, e.g., log or simple notification
-        },
+        onSuccess = {},
         onFailure = { exception ->
-          Log.e("UserViewModel", "Failed to update user: ${user.userId}", exception)
+          Log.e(
+              "com.github.se.cyrcle.model.user.UserViewModel",
+              "Failed to update user: ${user.userId}",
+              exception)
         })
   }
 
-  fun deleteUserById(userId: String) {
-    userRepository.deleteUserById(
-        userId,
-        onSuccess = {
-          // Handle success, e.g., log or notify
-        },
-        onFailure = { exception ->
-          Log.e("UserViewModel", "Failed to delete user by ID: $userId", exception)
-        })
+  /**
+   * Adds parking to the list of favorite parkings for the selected user.
+   *
+   * @param parkingId the ID of the parking to add to the list of favorite parkings
+   */
+  fun addFavoriteParkingToSelectedUser(parkingId: String) {
+    currentUser.value?.let { user ->
+      val updatedUser = user.copy(favoriteParkings = user.favoriteParkings + parkingId)
+      updateUser(updatedUser)
+      setCurrentUser(updatedUser)
+    }
   }
+
+  /**
+   * Removes parking from the list of favorite parkings for the selected user.
+   *
+   * @param parkingId the ID of the parking to remove from the list of favorite parkings
+   */
+  fun removeFavoriteParkingFromSelectedUser(parkingId: String) {
+    currentUser.value?.let { user ->
+      val updatedUser = user.copy(favoriteParkings = user.favoriteParkings - parkingId)
+      updateUser(updatedUser)
+      setCurrentUser(updatedUser)
+    }
+  }
+
   /*
-  fun updateUserLocation(userId: String, location: Location) {
-    viewModelScope.launch {
-      userRepository.updateUserLocation(
-          userId,
-          location,
-          onSuccess = {
-            // Handle success, e.g., log or notify
-          },
-          onFailure = { exception ->
-            Log.e("UserViewModel", "Failed to update location for user: $userId", exception)
-          })
-    }
-  }
-
-  fun getLastKnownLocation(userId: String, onResult: (Location?) -> Unit) {
-    viewModelScope.launch {
-      userRepository.getLastKnownLocation(
-          userId,
-          onSuccess = { location -> onResult(location) },
-          onFailure = { exception ->
-            Log.e("UserViewModel", "Failed to get last known location for user: $userId", exception)
-            onResult(null)
-          })
-    }
-  }
-
-  fun addFavoriteParking(userId: String, parkingId: String) {
-      userRepository.addFavoriteParking(
-          userId,
-          parkingId,
-          onSuccess = {
-
-          },
-          onFailure = { exception ->
-            Log.e("UserViewModel", "Failed to add favorite parking for user: $userId", exception)
-          })
-
-  }
-
-  fun removeFavoriteParking(userId: String, parkingId: String) {
-      userRepository.removeFavoriteParking(
-          userId,
-          parkingId,
-          onSuccess = {
-            // Handle success, e.g., log or notify
-          },
-          onFailure = { exception ->
-            Log.e("UserViewModel", "Failed to remove favorite parking for user: $userId", exception)
-          })
-  }
-
-
   fun getFavoriteParkings(userId: String, onResult: (List<String>) -> Unit) {
       userRepository.getFavoriteParkings(
           userId,
           onSuccess = { parkingIds -> onResult(parkingIds) },
           onFailure = { exception ->
-            Log.e("UserViewModel", "Failed to get favorite parkings for user: $userId", exception)
+            Log.e("com.github.se.cyrcle.model.user.UserViewModel", "Failed to get favorite parkings for user: $userId", exception)
             onResult(emptyList())
           })
   }
-
-
    */
 
   companion object {
