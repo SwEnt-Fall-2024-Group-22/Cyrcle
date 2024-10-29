2a16be6c1e7ccd3448b628ce52e6ee70fb4f9e6d
Mattéo Santini <mattsantini2003@gmail.com>
2024-10-29 19:53:45 +0100

    ci: fix properties declaration

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,14 @@ plugins {
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.ktfmt) apply false
     alias(libs.plugins.gms) apply false
-
     alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.sonar)
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "SwEnt-Fall-2024-Group-22_Cyrcle")
+        property("sonar.organization", "swent-fall-2024-group-22")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
