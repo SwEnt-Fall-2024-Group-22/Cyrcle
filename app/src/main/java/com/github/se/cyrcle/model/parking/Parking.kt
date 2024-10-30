d38e0f1e5ee49b89b9e0e5873c0dbf70647b95e7
LouisVasseur <louisvasseur996@gmail.com>
2024-10-30 18:15:22 +0100

    feat: connect averageScore attribute from Reviews to Card Screen

--- app/src/main/java/com/github/se/cyrcle/model/parking/Parking.kt
+++ app/src/main/java/com/github/se/cyrcle/model/parking/Parking.kt
@@ -15,6 +15,8 @@ import com.mapbox.geojson.Point
  * @property protection Protection of the parking spot from the weather.
  * @property price Price of the parking spot. The price is in the currency of the country where the
  *   parking spot is located. 0 if the parking spot is free.
+ * @property nbReviews Number of Reviews for this Parking spot as of now.
+ * @property avgScore Average Review Score for this Parking spot as of now.
  */
 data class Parking(
     val uid: String,
@@ -26,38 +28,39 @@ data class Parking(
     val rackType: ParkingRackType,
     val protection: ParkingProtection,
     val price: Double,
-    val hasSecurity: Boolean
-    // TODO: Add list of reviews when implemented
+    val hasSecurity: Boolean,
+    var nbReviews: Int = 0,
+    var avgScore: Double = 0.0
 )
 
 interface ParkingAttribute {
-  val description: String
+    val description: String
 }
 /** Enum class representing the capacity of a parking spot. */
 enum class ParkingCapacity(override val description: String) : ParkingAttribute {
-  XSMALL("Less than 10 spots"),
-  SMALL("10-25 spots"),
-  MEDIUM("26-50 spots"),
-  LARGE("51-100 spots"),
-  XLARGE("More than 100 spots")
+    XSMALL("Less than 10 spots"),
+    SMALL("10-25 spots"),
+    MEDIUM("26-50 spots"),
+    LARGE("51-100 spots"),
+    XLARGE("More than 100 spots")
 }
 
 /** Enum class representing the type of rack in a parking spot. */
 enum class ParkingRackType(override val description: String) : ParkingAttribute {
-  TWO_TIER("Two-tier rack"),
-  U_RACK("U-Rack"),
-  VERTICAL("Vertical rack"),
-  WAVE("Wave rack"),
-  WALL_BUTTERFLY("Wall butterfly rack"),
-  POST_AND_RING("Post and ring rack"),
-  GRID("Grid rack"),
-  OTHER("Other type of rack")
+    TWO_TIER("Two-tier rack"),
+    U_RACK("U-Rack"),
+    VERTICAL("Vertical rack"),
+    WAVE("Wave rack"),
+    WALL_BUTTERFLY("Wall butterfly rack"),
+    POST_AND_RING("Post and ring rack"),
+    GRID("Grid rack"),
+    OTHER("Other type of rack")
 }
 
 enum class ParkingProtection(override val description: String) : ParkingAttribute {
-  INDOOR("Indoor"),
-  COVERED("Covered"),
-  NONE("Exposed")
+    INDOOR("Indoor"),
+    COVERED("Covered"),
+    NONE("Exposed")
 }
 
 data class Location(
@@ -67,23 +70,23 @@ data class Location(
     val bottomLeft: Point?,
     val bottomRight: Point?,
 ) {
-  constructor(
-      topLeft: Point,
-      topRight: Point,
-      bottomRight: Point,
-      bottomLeft: Point
-  ) : this(
-      Point.fromLngLat(
-          (topLeft.longitude() + bottomRight.longitude()) / 2,
-          (topLeft.latitude() + bottomRight.latitude()) / 2),
-      topLeft,
-      topRight,
-      bottomLeft,
-      bottomRight)
+    constructor(
+        topLeft: Point,
+        topRight: Point,
+        bottomRight: Point,
+        bottomLeft: Point
+    ) : this(
+        Point.fromLngLat(
+            (topLeft.longitude() + bottomRight.longitude()) / 2,
+            (topLeft.latitude() + bottomRight.latitude()) / 2),
+        topLeft,
+        topRight,
+        bottomLeft,
+        bottomRight)
 
-  constructor(center: Point) : this(center, null, null, null, null)
+    constructor(center: Point) : this(center, null, null, null, null)
 
-  constructor(
-      listPoints: List<Point>
-  ) : this(listPoints[0], listPoints[1], listPoints[2], listPoints[3])
+    constructor(
+        listPoints: List<Point>
+    ) : this(listPoints[0], listPoints[1], listPoints[2], listPoints[3])
 }
--- app/src/main/java/com/github/se/cyrcle/ui/card/CardScreen.kt
+++ app/src/main/java/com/github/se/cyrcle/ui/card/CardScreen.kt
@@ -39,151 +39,158 @@ fun CardScreen(
     navigationActions: NavigationActions,
     parkingViewModel: ParkingViewModel = viewModel(factory = ParkingViewModel.Factory)
 ) {
-  val selectedParking =
-      parkingViewModel.selectedParking.collectAsState().value
-          ?: return Text(text = "No parking selected. Should not happen")
+    val selectedParking =
+        parkingViewModel.selectedParking.collectAsState().value
+            ?: return Text(text = "No parking selected. Should not happen")
 
-  Scaffold(
-      topBar = {
-        TopAppBar(navigationActions, "Description of ${selectedParking.optName?: "Parking"}")
-      },
-      modifier = Modifier.testTag("CardScreen")) {
+    Scaffold(
+        topBar = {
+            TopAppBar(navigationActions, "Description of ${selectedParking.optName?: "Parking"}")
+        },
+        modifier = Modifier.testTag("CardScreen")) {
         Box(
             modifier =
-                Modifier.fillMaxSize()
-                    .background(MaterialTheme.colorScheme.background)
-                    .testTag("CardScreenBox") // Test tag for main container
-            ) {
-              Column(
-                  modifier = Modifier.fillMaxSize().padding(it),
-                  horizontalAlignment = Alignment.CenterHorizontally,
-                  verticalArrangement = Arrangement.SpaceBetween) {
-                    // Display a row of images using LazyRow
-                    LazyRow(
-                        modifier =
-                            Modifier.fillMaxWidth()
-                                .padding(8.dp)
-                                .testTag("ParkingImagesRow"), // Test tag for image row
-                        horizontalArrangement = Arrangement.spacedBy(4.dp)) {
-                          items(selectedParking.images.size) { index ->
-                            AsyncImage(
-                                model = selectedParking.images[index],
-                                contentDescription = "Image $index",
-                                modifier =
-                                    Modifier.size(170.dp)
-                                        .padding(2.dp)
-                                        .fillMaxWidth()
-                                        .testTag("ParkingImage$index"), // Test tag for each image
-                                contentScale = ContentScale.Crop)
-                          }
+            Modifier.fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+                .testTag("CardScreenBox") // Test tag for main container
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize().padding(it),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween) {
+                // Display a row of images using LazyRow
+                LazyRow(
+                    modifier =
+                    Modifier.fillMaxWidth()
+                        .padding(8.dp)
+                        .testTag("ParkingImagesRow"), // Test tag for image row
+                    horizontalArrangement = Arrangement.spacedBy(4.dp)) {
+                    items(selectedParking.images.size) { index ->
+                        AsyncImage(
+                            model = selectedParking.images[index],
+                            contentDescription = "Image $index",
+                            modifier =
+                            Modifier.size(170.dp)
+                                .padding(2.dp)
+                                .fillMaxWidth()
+                                .testTag("ParkingImage$index"), // Test tag for each image
+                            contentScale = ContentScale.Crop)
+                    }
+                }
+
+                // Column for parking info such as capacity, rack type, protection, etc.
+                Column(
+                    modifier =
+                    Modifier.fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                        .testTag("InfoColumn"), // Test tag for info column
+                    verticalArrangement = Arrangement.spacedBy(8.dp)) {
+                    // Row for displaying capacity and rack type
+                    Row(
+                        modifier = Modifier.fillMaxWidth().testTag("RowCapacityRack"),
+                        horizontalArrangement = Arrangement.SpaceBetween) {
+                        Column(modifier = Modifier.weight(1f).testTag("CapacityColumn")) {
+                            Text(text = "Capacity:", fontWeight = FontWeight.Bold)
+                            Text(
+                                text = selectedParking.capacity.description,
+                                color = Color.Gray)
                         }
-
-                    // Column for parking info such as capacity, rack type, protection, etc.
-                    Column(
-                        modifier =
-                            Modifier.fillMaxWidth()
-                                .padding(horizontal = 16.dp)
-                                .testTag("InfoColumn"), // Test tag for info column
-                        verticalArrangement = Arrangement.spacedBy(8.dp)) {
-                          // Row for displaying capacity and rack type
-                          Row(
-                              modifier = Modifier.fillMaxWidth().testTag("RowCapacityRack"),
-                              horizontalArrangement = Arrangement.SpaceBetween) {
-                                Column(modifier = Modifier.weight(1f).testTag("CapacityColumn")) {
-                                  Text(text = "Capacity:", fontWeight = FontWeight.Bold)
-                                  Text(
-                                      text = selectedParking.capacity.description,
-                                      color = Color.Gray)
-                                }
-                                Column(modifier = Modifier.weight(1f).testTag("RackTypeColumn")) {
-                                  Text(text = "Rack Type:", fontWeight = FontWeight.Bold)
-                                  Text(
-                                      text = selectedParking.rackType.description,
-                                      color = Color.Gray)
-                                }
-                              }
-
-                          // Row for displaying protection and price
-                          Row(
-                              modifier = Modifier.fillMaxWidth().testTag("RowProtectionPrice"),
-                              horizontalArrangement = Arrangement.SpaceBetween) {
-                                Column(modifier = Modifier.weight(1f).testTag("ProtectionColumn")) {
-                                  Text(text = "Protection:", fontWeight = FontWeight.Bold)
-                                  Text(
-                                      text = selectedParking.protection.description,
-                                      color = Color.Gray)
-                                }
-                                Column(modifier = Modifier.weight(1f).testTag("PriceColumn")) {
-                                  Text(text = "Price:", fontWeight = FontWeight.Bold)
-                                  val price = selectedParking.price
-                                  Text(
-                                      text = if (price == 0.0) "Free" else "$price",
-                                      color = Color.Gray)
-                                }
-                              }
-
-                          // Row for displaying if security is present
-                          Row(
-                              modifier = Modifier.fillMaxWidth().testTag("RowSecurity"),
-                              horizontalArrangement = Arrangement.SpaceBetween) {
-                                Column(modifier = Modifier.weight(1f).testTag("SecurityColumn")) {
-                                  Text(text = "Security Present:", fontWeight = FontWeight.Bold)
-                                  Text(
-                                      text = if (selectedParking.hasSecurity) "Yes" else "No",
-                                      color = Color.Gray)
-                                }
-                              }
+                        Column(modifier = Modifier.weight(1f).testTag("RackTypeColumn")) {
+                            Text(text = "Rack Type:", fontWeight = FontWeight.Bold)
+                            Text(
+                                text = selectedParking.rackType.description,
+                                color = Color.Gray)
                         }
+                    }
 
-                    // Column for action buttons like "Show in Map", "Add A Review", and "Report"
-                    Column(
-                        modifier =
-                            Modifier.fillMaxWidth()
-                                .padding(horizontal = 16.dp)
-                                .testTag("ButtonsColumn"), // Test tag for buttons column
-                        verticalArrangement = Arrangement.spacedBy(16.dp)) {
-                          Button(
-                              onClick = { /* Handle Return to Map */},
-                              modifier =
-                                  Modifier.fillMaxWidth()
-                                      .height(40.dp)
-                                      .testTag(
-                                          "ShowInMapButton"), // Test tag for Show in Map button
-                              colors =
-                                  ButtonDefaults.buttonColors(
-                                      containerColor = MaterialTheme.colorScheme.primary,
-                                      contentColor = MaterialTheme.colorScheme.secondary)) {
-                                Text(text = "Show in Map")
-                              }
-
-                          Button(
-                              onClick = { navigationActions.navigateTo(Screen.REVIEW) },
-                              modifier =
-                                  Modifier.fillMaxWidth()
-                                      .height(40.dp)
-                                      .testTag("AddReviewButton"), // Test tag for Add Review button
-                              colors =
-                                  ButtonDefaults.buttonColors(
-                                      containerColor = MaterialTheme.colorScheme.primary,
-                                      contentColor = MaterialTheme.colorScheme.secondary)) {
-                                Text(text = "Add A Review")
-                              }
-
-                          Button(
-                              onClick = {},
-                              modifier =
-                                  Modifier.height(30.dp)
-                                      .testTag("ReportButton"), // Test tag for Report button
-                              colors =
-                                  ButtonDefaults.buttonColors(
-                                      containerColor = Red,
-                                      contentColor = MaterialTheme.colorScheme.secondary)) {
-                                Text(text = "Report")
-                              }
+                    // Row for displaying protection and price
+                    Row(
+                        modifier = Modifier.fillMaxWidth().testTag("RowProtectionPrice"),
+                        horizontalArrangement = Arrangement.SpaceBetween) {
+                        Column(modifier = Modifier.weight(1f).testTag("ProtectionColumn")) {
+                            Text(text = "Protection:", fontWeight = FontWeight.Bold)
+                            Text(
+                                text = selectedParking.protection.description,
+                                color = Color.Gray)
                         }
+                        Column(modifier = Modifier.weight(1f).testTag("PriceColumn")) {
+                            Text(text = "Price:", fontWeight = FontWeight.Bold)
+                            val price = selectedParking.price
+                            Text(
+                                text = if (price == 0.0) "Free" else "$price",
+                                color = Color.Gray)
+                        }
+                    }
 
-                    Spacer(modifier = Modifier.height(16.dp))
-                  }
+                    // Row for displaying if security is present
+                    Row(
+                        modifier = Modifier.fillMaxWidth().testTag("RowSecurity"),
+                        horizontalArrangement = Arrangement.SpaceBetween) {
+                        Column(modifier = Modifier.weight(1f).testTag("SecurityColumn")) {
+                            Text(text = "Security Present:", fontWeight = FontWeight.Bold)
+                            Text(
+                                text = if (selectedParking.hasSecurity) "Yes" else "No",
+                                color = Color.Gray)
+                        }
+                        Column(modifier = Modifier.weight(1f).testTag("AverageRatingColumn")) {
+                            Text(text = "Current Rating", fontWeight = FontWeight.Bold)
+                            Text(
+                                text = selectedParking.avgScore.toString(),
+                                color = Color.Gray)
+                        }
+                    }
+
+                }
+
+                // Column for action buttons like "Show in Map", "Add A Review", and "Report"
+                Column(
+                    modifier =
+                    Modifier.fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                        .testTag("ButtonsColumn"), // Test tag for buttons column
+                    verticalArrangement = Arrangement.spacedBy(16.dp)) {
+                    Button(
+                        onClick = { /* Handle Return to Map */},
+                        modifier =
+                        Modifier.fillMaxWidth()
+                            .height(40.dp)
+                            .testTag(
+                                "ShowInMapButton"), // Test tag for Show in Map button
+                        colors =
+                        ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.primary,
+                            contentColor = MaterialTheme.colorScheme.secondary)) {
+                        Text(text = "Show in Map")
+                    }
+
+                    Button(
+                        onClick = { navigationActions.navigateTo(Screen.REVIEW) },
+                        modifier =
+                        Modifier.fillMaxWidth()
+                            .height(40.dp)
+                            .testTag("AddReviewButton"), // Test tag for Add Review button
+                        colors =
+                        ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.primary,
+                            contentColor = MaterialTheme.colorScheme.secondary)) {
+                        Text(text = "Add A Review")
+                    }
+
+                    Button(
+                        onClick = {},
+                        modifier =
+                        Modifier.height(30.dp)
+                            .testTag("ReportButton"), // Test tag for Report button
+                        colors =
+                        ButtonDefaults.buttonColors(
+                            containerColor = Red,
+                            contentColor = MaterialTheme.colorScheme.secondary)) {
+                        Text(text = "Report")
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
             }
-      }
+        }
+    }
 }
