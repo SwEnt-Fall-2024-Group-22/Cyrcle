69285bd28e687df6e256b15e7b478375ab6283c7
LouisVasseur <louisvasseur996@gmail.com>
2024-10-31 11:37:35 +0100

    fix: fix slider values displaying too many numbers on some devices

--- app/src/main/java/com/github/se/cyrcle/ui/review/ReviewScreen.kt
+++ app/src/main/java/com/github/se/cyrcle/ui/review/ReviewScreen.kt
@@ -97,7 +97,7 @@ fun ReviewScreen(
               modifier = Modifier.padding(top = 8.dp).testTag("ExperienceText"))
 
           // Slider with step granularity of 0.5
-          Text(text = "Rating: ${2*(sliderValue.toInt())/2.0}", style = MaterialTheme.typography.bodyLarge)
+          Text(text = "Rating: ${sliderValue.toDouble()}", style = MaterialTheme.typography.bodyLarge)
           Slider(
               value = sliderValue,
               onValueChange = { newValue -> sliderValue = newValue },
