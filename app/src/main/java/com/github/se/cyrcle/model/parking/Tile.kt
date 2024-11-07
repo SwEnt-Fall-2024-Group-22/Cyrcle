82b0a91c55113230e8223b7c1ccb9c2062802b0d
Perazzolo Sean <sean.perazzolo@gmail.com>
2024-11-07 20:00:33 +0100

    fix: wrong tiles fetched from rep
    Multiple mistakes have been spoted in this function thanks to the viewmodel test.
    
    A floating point error made the repo fetch too much tile, not necessary
    The bounds were non strict when strict bound could be used to reduce number of tile loaded

--- app/src/main/java/com/github/se/cyrcle/model/parking/Tile.kt
+++ app/src/main/java/com/github/se/cyrcle/model/parking/Tile.kt
@@ -2,6 +2,8 @@ package com.github.se.cyrcle.model.parking
 
 import com.mapbox.geojson.Point
 import com.mapbox.turf.TurfMeasurement
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 const val TILE_SIZE = 0.1
 /**
@@ -38,17 +40,29 @@ data class Tile(val bottomLeft: Point, val topRight: Point) {
      */
     fun getAllTilesInRectangle(bottomLeft: Point, topRight: Point): Set<Tile> {
       val tiles = mutableSetOf<Tile>()
-      for (x in
-          (bottomLeft.longitude() / TILE_SIZE).toInt()..(topRight.longitude() / TILE_SIZE)
-                  .toInt()) {
-        for (y in
-            (bottomLeft.latitude() / TILE_SIZE).toInt()..(topRight.latitude() / TILE_SIZE)
-                    .toInt()) {
-          val currentTile =
-              Tile(
-                  Point.fromLngLat(x * TILE_SIZE, y * TILE_SIZE),
-                  Point.fromLngLat((x + 1) * TILE_SIZE, (y + 1) * TILE_SIZE))
-          tiles.add(roundTiles(currentTile))
+      // round with a formatter to the 7 digits the bottomLeft and topRight
+
+      /* round down bottomLeft to the nearest TILE_SIZE
+      Note : The BigDecimal is used to avoid floating point errors i.e 7.01 turning into 7.0099999999999998
+      Making the repo look for the tile containing 7.0099999999999998 when not needed
+       */
+      val bottomLeftX =
+          BigDecimal(bottomLeft.longitude() / TILE_SIZE).setScale(7, RoundingMode.HALF_EVEN).toInt()
+      val bottomLeftY =
+          BigDecimal(bottomLeft.latitude() / TILE_SIZE).setScale(7, RoundingMode.HALF_EVEN).toInt()
+      // round up topRight to the nearest TILE_SIZE
+      val topRightX =
+          BigDecimal(topRight.longitude() / TILE_SIZE).setScale(0, RoundingMode.UP).toInt()
+      val topRightY =
+          BigDecimal(topRight.latitude() / TILE_SIZE).setScale(0, RoundingMode.UP).toInt()
+
+      for (x in bottomLeftX until topRightX) {
+        for (y in bottomLeftY until topRightY) {
+          tiles.add(
+              roundTiles(
+                  Tile(
+                      Point.fromLngLat(x * TILE_SIZE, y * TILE_SIZE),
+                      Point.fromLngLat((x + 1) * TILE_SIZE, (y + 1) * TILE_SIZE))))
         }
       }
       return tiles
