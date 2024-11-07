1ed470ad38fffaa65a234f28fe4e9f3b61ffa10c
LouisVasseur <louisvasseur996@gmail.com>
2024-11-07 20:23:00 +0100

    feat: increase viewModels to add/edit Review

--- app/src/main/java/com/github/se/cyrcle/model/parking/ParkingViewModel.kt
+++ app/src/main/java/com/github/se/cyrcle/model/parking/ParkingViewModel.kt
@@ -146,12 +146,21 @@ class ParkingViewModel(
    * @param newScore: score of the new review to add
    * @param parking: Parking to update (selectedParking by default, should never be null)
    */
-  fun updateReviewScore(newScore: Double, parking: Parking = selectedParking.value!!) {
-    parking.avgScore =
-        (100 * ((parking.avgScore * parking.nbReviews) + newScore) / (parking.nbReviews + 1))
-            .toInt() / 100.00
-    parking.nbReviews += 1
-    parkingRepository.updateParking(parking, {}, {})
+  fun updateReviewScore(
+      newScore: Double,
+      oldScore: Double = 0.0,
+      parking: Parking = selectedParking.value!!,
+      isNewReview: Boolean
+  ) {
+    if (isNewReview) {
+      parking.avgScore =
+          (100 * ((parking.avgScore * parking.nbReviews) + newScore) / (parking.nbReviews + 1))
+              .toInt() / 100.00
+      parking.nbReviews += 1
+      parkingRepository.updateParking(parking, {}, {})
+    } else {
+      parking.avgScore += (oldScore - newScore) / parking.nbReviews
+    }
   }
 
   // create factory (imported from bootcamp)
