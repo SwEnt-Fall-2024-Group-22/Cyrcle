94925dbf2fa44187ff82faab9257f7825effc320
Perazzolo Sean <sean.perazzolo@gmail.com>
2024-11-07 20:01:56 +0100

    feat: testing suite for the new getParkingsInRadius

--- app/src/main/java/com/github/se/cyrcle/model/parking/TestInstancesParking.kt
+++ app/src/main/java/com/github/se/cyrcle/model/parking/TestInstancesParking.kt
@@ -64,4 +64,30 @@ object TestInstancesParking {
           ParkingProtection.COVERED,
           0.0,
           true)
+  val parking4 =
+      Parking(
+          "Test_spot_4",
+          "",
+          null,
+          Location(Point.fromLngLat(7.111, 47.111)),
+          listOf(
+              "https://upload.wikimedia.org/wikipedia/commons/7/78/%22G%C3%A4nsemarkt%22_in_Amance_-_panoramio.jpg"),
+          ParkingCapacity.LARGE,
+          ParkingRackType.TWO_TIER,
+          ParkingProtection.COVERED,
+          0.0,
+          true)
+  val parking5 =
+      Parking(
+          "Test_spot_5",
+          "",
+          null,
+          Location(Point.fromLngLat(7.112, 47.112)),
+          listOf(
+              "https://upload.wikimedia.org/wikipedia/commons/7/78/%22G%C3%A4nsemarkt%22_in_Amance_-_panoramio.jpg"),
+          ParkingCapacity.LARGE,
+          ParkingRackType.TWO_TIER,
+          ParkingProtection.COVERED,
+          0.0,
+          true)
 }
