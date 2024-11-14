426f391566a899c0f79014b21a849e1df3e6bfeb
André Peiry <andre.peiry@epfl.ch>
2024-11-14 13:34:42 +0100

    test: revise an InputText call with new arguments

--- app/src/androidTest/java/com/github/se/cyrcle/ui/theme/atoms/InputTest.kt
+++ app/src/androidTest/java/com/github/se/cyrcle/ui/theme/atoms/InputTest.kt
@@ -10,7 +10,6 @@ import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.github.se.cyrcle.ui.theme.White
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
