2063fdb4027c146ecfe8f8fffe9db1ca8708f76e
André Peiry <andre.peiry@epfl.ch>
2024-11-14 19:47:17 +0100

    feature: add the Wallet class for user details
    Define the Wallet class to represent the currencies that a user owns.

--- app/src/main/java/com/github/se/cyrcle/model/user/Wallet.kt
+++ app/src/main/java/com/github/se/cyrcle/model/user/Wallet.kt
@@ -1,24 +1,59 @@
 package com.github.se.cyrcle.model.user
 
+/**
+ * A type alias for the amount of coins a user has. Allow more flexibility in the future if we want
+ * to change the type of the coins.
+ */
 typealias Coin = Int
 
 /**
- * TODO
+ * A class that represent a [Coin] container (hence the name), which can be used to store and
+ * manipulate the amount of coins a user has.
  */
-class Wallet {
+class Wallet(private var coins: Coin = 0) {
 
-  private var coins: Coin = 0
-
-  fun isSolvable(coinsToHave: Coin): Boolean {
-    return coins >= coinsToHave
+  /**
+   * Add amount of coins to the wallet.
+   *
+   * @param amount the amount of coins to add. Must be positive.
+   */
+  fun creditCoins(amount: Coin) {
+    if (amount < 0) throw IllegalArgumentException("Amount must be positive")
+    coins += amount
   }
 
   /**
-   * TODO
+   * Remove amount of coins to the wallet. This function does not check if the wallet is solvable.
+   *
+   * @param amount the amount of coins to remove. Must be positive.
    */
-    companion object {
-        fun empty(): Wallet {
-            return Wallet()
-        }
+  fun debitCoins(amount: Coin) {
+    if (amount < 0) throw IllegalArgumentException("Amount must be positive")
+    coins -= amount
+  }
+
+  /**
+   * Return whether the user is solvable. A user is solvable if, after debiting the amount, the
+   * amount of coins in the wallet is still superior to the credit threshold.
+   * This function does not modify the value of the coins in the wallet, it's a simple projection.
+   */
+  fun isSolvable(amount: Coin, creditThreshold: Coin): Boolean {
+    if (amount < 0) throw IllegalArgumentException("Amount must be positive")
+    return (coins - amount) > creditThreshold
+  }
+
+  /**
+   * Return the amount of coins currently in the mallet. WARNING: This method does not modify the
+   * value of the coins in the wallet.
+   */
+  fun getCoins(): Coin {
+    return coins
+  }
+
+  /** Creates an empty [Wallet]. It contains no coins. */
+  companion object {
+    fun empty(): Wallet {
+      return Wallet()
     }
+  }
 }
