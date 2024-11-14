ecf6f19df7a5e8494bf60decf2a683269b98ecd3
LouisVasseur <louisvasseur996@gmail.com>
2024-11-14 20:06:29 +0100

    format: format using ktfmtFormat

--- app/src/androidTest/java/com/github/se/cyrcle/ui/review/ReviewScreenTest.kt
+++ app/src/androidTest/java/com/github/se/cyrcle/ui/review/ReviewScreenTest.kt
@@ -4,7 +4,6 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextContains
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
 import androidx.compose.ui.test.performTouchInput
@@ -85,7 +84,6 @@ class ReviewScreenTest {
     }
     // Perform actions on the slider
     composeTestRule.onNodeWithTag("Slider").performTouchInput { swipeRight() }
-
   }
 
   @Test
