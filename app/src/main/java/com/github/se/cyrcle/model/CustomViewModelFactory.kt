2c70d59598564b9b575c0a2c3db2bd16418961e4
Pijukai <julien.barozet-golbery@epfl.ch>
2024-11-20 15:28:35 +0100

    add (doc): complete doc in CustomViewModelFactory

--- app/src/main/java/com/github/se/cyrcle/model/CustomViewModelFactory.kt
+++ app/src/main/java/com/github/se/cyrcle/model/CustomViewModelFactory.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.ViewModelProvider
 
 class CustomViewModelFactory<T : ViewModel>(private val creator: () -> T) :
     ViewModelProvider.Factory {
+
+  // Factory to unify the different factory of view Models
   override fun <T : ViewModel> create(modelClass: Class<T>): T {
     @Suppress("UNCHECKED_CAST") return creator() as T
   }
