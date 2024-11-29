c693471531ef5cc2bed5b90bd5526acc8d7d1d2e
Perazzolo Sean <sean.perazzolo@gmail.com>
2024-11-29 18:54:02 +0100

    feat: add 2 empty screens for zone management

---
+++ app/src/main/java/com/github/se/cyrcle/ui/zone/ZoneManagerScreen.kt
@@ -0,0 +1,11 @@
+package com.github.se.cyrcle.ui.zone
+
+import androidx.compose.runtime.Composable
+import com.github.se.cyrcle.ui.navigation.NavigationActions
+import com.github.se.cyrcle.ui.theme.atoms.Text
+
+/** Screen where users can manage their downloaded zones. and access the zone selection screen. */
+@Composable
+fun ZoneManagerScreen(navigationActions: NavigationActions) {
+  Text("Zone Manager")
+}
---
+++ app/src/main/java/com/github/se/cyrcle/ui/zone/ZoneSelectionScreen.kt
@@ -0,0 +1,11 @@
+package com.github.se.cyrcle.ui.zone
+
+import androidx.compose.runtime.Composable
+import com.github.se.cyrcle.ui.navigation.NavigationActions
+import com.github.se.cyrcle.ui.theme.atoms.Text
+
+/** Screen where users can select a new zone to download. */
+@Composable
+fun ZoneSelectionScreen(navigationActions: NavigationActions) {
+  Text("New Zone Selection")
+}
