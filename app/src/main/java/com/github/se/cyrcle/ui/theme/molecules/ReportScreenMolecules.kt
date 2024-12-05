7f7005059ea9bdccfe2e4e4bac07c15aa3c2a55c
LouisVasseur <louisvasseur996@gmail.com>
2024-12-05 17:14:13 +0100

    feat: add Image to the possible ReportedObjects

--- app/src/main/java/com/github/se/cyrcle/ui/theme/molecules/ReportScreenMolecules.kt
+++ app/src/main/java/com/github/se/cyrcle/ui/theme/molecules/ReportScreenMolecules.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.github.se.cyrcle.R
+import com.github.se.cyrcle.model.parking.ImageReportReason
 import com.github.se.cyrcle.model.parking.ParkingReportReason
 import com.github.se.cyrcle.model.report.ReportedObjectType
 import com.github.se.cyrcle.model.review.ReviewReportReason
