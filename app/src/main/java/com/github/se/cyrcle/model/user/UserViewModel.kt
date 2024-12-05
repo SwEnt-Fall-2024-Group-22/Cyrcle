4e859a6d3eacbe1b7bf32df8c19893428688c55b
LouisVasseur <louisvasseur996@gmail.com>
2024-12-05 17:23:17 +0100

    fix: add ReportedImages to users

--- app/src/main/java/com/github/se/cyrcle/model/user/UserViewModel.kt
+++ app/src/main/java/com/github/se/cyrcle/model/user/UserViewModel.kt
@@ -215,6 +215,15 @@ class UserViewModel(
     }
   }
 
+    fun addReportedImageToSelectedUser(image: String) {
+        currentUser.value?.let { user ->
+            val updatedDetails =
+                user.details?.copy(reportedReviews = user.details.reportedImages + image)
+            val updatedUser = user.copy(details = updatedDetails)
+            updateUser(updatedUser) { _reportedParkings.value += image }
+        }
+    }
+
   /**
    * Removes parking from the list of favorite parkings for the selected user and updates the
    * favorite parkings state.
