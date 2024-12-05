592c726a79f8f04d194fb4a37663f14bf8c82719
LouisVasseur <louisvasseur996@gmail.com>
2024-12-05 23:59:38 +0100

    fix: add more reporting logic to images

---
+++ app/src/main/java/com/github/se/cyrcle/model/parking/online/ParkingImageListAdapter.kt
@@ -0,0 +1,22 @@
+package com.github.se.cyrcle.model.parking.online
+
+import androidx.room.TypeConverter
+import com.github.se.cyrcle.model.parking.ParkingImage
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class ParkingImageListAdapter {
+
+    private val gson = Gson()
+
+    @TypeConverter
+    fun fromParkingImageList(value: List<ParkingImage>?): String? {
+        return gson.toJson(value)
+    }
+
+    @TypeConverter
+    fun toParkingImageList(value: String?): List<ParkingImage>? {
+        val listType = object : TypeToken<List<ParkingImage>>() {}.type
+        return gson.fromJson(value, listType)
+    }
+}
