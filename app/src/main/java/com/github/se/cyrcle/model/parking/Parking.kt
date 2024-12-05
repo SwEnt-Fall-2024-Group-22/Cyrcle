dc39c539f2211f62e248d3b7376bb34a0db87469
LouisVasseur <louisvasseur996@gmail.com>
2024-12-05 17:11:40 +0100

    feat: adapt Parking to implement Images

--- app/src/main/java/com/github/se/cyrcle/model/parking/Parking.kt
+++ app/src/main/java/com/github/se/cyrcle/model/parking/Parking.kt
@@ -44,10 +44,17 @@ data class Parking(
     var nbMaxSeverityReports: Int = 0,
     val tile: Tile = TileUtils.getTileFromPoint(location.center)
 ) {
-  companion object {
-    const val TABLE_NAME = "parkings"
-  }
+    companion object {
+        const val TABLE_NAME = "parkings"
+    }
 }
+
+data class ParkingImage(
+    val uid: String = "",
+    val userId: String = "",
+    val parking: String = "",
+    val url: String = ""
+)
 
 data class ParkingReport(
     val uid: String = "",
@@ -57,44 +64,63 @@ data class ParkingReport(
     val description: String = ""
 )
 
+data class ImageReport(
+    val uid: String = "",
+    val reason: ImageReportReason = ImageReportReason.OTHER,
+    val userId: String = "",
+    val parking: String = "",
+    val description: String = ""
+)
+
 interface ParkingAttribute : DropDownableEnum
 
 /** Enum class representing the capacity of a parking spot. */
 enum class ParkingCapacity(override val description: String) : ParkingAttribute {
-  XSMALL("Less than 10 spots"),
-  SMALL("10-25 spots"),
-  MEDIUM("26-50 spots"),
-  LARGE("51-100 spots"),
-  XLARGE("More than 100 spots")
+    XSMALL("Less than 10 spots"),
+    SMALL("10-25 spots"),
+    MEDIUM("26-50 spots"),
+    LARGE("51-100 spots"),
+    XLARGE("More than 100 spots")
 }
 
 // Reasons for reporting a Parking
 enum class ParkingReportReason(override val description: String, val severity: Int) :
     DropDownableEnum {
-  POOR_MAINTENANCE("This Spot has Poor Maintenance", 1),
-  INACCESSIBLE("This Spot is Inaccessible", 2),
-  ILLEGAL_SPOT("This Spot is in an Illegal Place", 3),
-  SAFETY_CONCERN("This Spot is in an Dangerous Place", 3),
-  INEXISTANT("This Spot does Not Exist", 3),
-  OTHER("OTHER", 1)
+    POOR_MAINTENANCE("This Spot has Poor Maintenance", 1),
+    INACCESSIBLE("This Spot is Inaccessible", 2),
+    ILLEGAL_SPOT("This Spot is in an Illegal Place", 3),
+    SAFETY_CONCERN("This Spot is in an Dangerous Place", 3),
+    INEXISTANT("This Spot does Not Exist", 3),
+    OTHER("OTHER", 1)
+}
+
+// Reasons for reporting a Parking
+enum class ImageReportReason(override val description: String, val severity: Int) :
+    DropDownableEnum {
+    USELESS("This Image isn't useful ", 1),
+    MISLEADING("This Image doesn't Represent the Parking", 2),
+    WRONG("This Image isn't related to the Parking", 3),
+    SAFETY_CONCERN("I'm in this picture and I don't like it", 3),
+    ILLEGAL_CONTENT("This Image contains illegal content", 3),
+    OTHER("OTHER", 1)
 }
 
 /** Enum class representing the type of rack in a parking spot. */
 enum class ParkingRackType(override val description: String) : ParkingAttribute {
-  TWO_TIER("Two-tier rack"),
-  U_RACK("U-Rack"),
-  VERTICAL("Vertical rack"),
-  WAVE("Wave rack"),
-  WALL_BUTTERFLY("Wall butterfly rack"),
-  POST_AND_RING("Post and ring rack"),
-  GRID("Grid rack"),
-  OTHER("Other type of rack")
+    TWO_TIER("Two-tier rack"),
+    U_RACK("U-Rack"),
+    VERTICAL("Vertical rack"),
+    WAVE("Wave rack"),
+    WALL_BUTTERFLY("Wall butterfly rack"),
+    POST_AND_RING("Post and ring rack"),
+    GRID("Grid rack"),
+    OTHER("Other type of rack")
 }
 
 enum class ParkingProtection(override val description: String) : ParkingAttribute {
-  INDOOR("Indoor"),
-  COVERED("Covered"),
-  NONE("Exposed")
+    INDOOR("Indoor"),
+    COVERED("Covered"),
+    NONE("Exposed")
 }
 
 data class Location(
@@ -105,61 +131,61 @@ data class Location(
     val bottomRight: Point?,
 ) {
 
-  /**
-   * Convert the location to a polygon.
-   *
-   * @return The polygon representing the location.
-   */
-  fun toPolygon(): Polygon {
-    if (topLeft == null || topRight == null || bottomLeft == null || bottomRight == null) {
-      Log.e("Location", "Cannot convert location to polygon, some corners are null")
-      return Polygon.fromLngLats(listOf(listOf()))
+    /**
+     * Convert the location to a polygon.
+     *
+     * @return The polygon representing the location.
+     */
+    fun toPolygon(): Polygon {
+        if (topLeft == null || topRight == null || bottomLeft == null || bottomRight == null) {
+            Log.e("Location", "Cannot convert location to polygon, some corners are null")
+            return Polygon.fromLngLats(listOf(listOf()))
+        }
+        return Polygon.fromLngLats(listOf(listOf(topLeft, topRight, bottomRight, bottomLeft, topLeft)))
     }
-    return Polygon.fromLngLats(listOf(listOf(topLeft, topRight, bottomRight, bottomLeft, topLeft)))
-  }
 
-  /**
-   * Compute the area of the location in square meters. The area is computed using the Turf
-   * Measurement library.
-   *
-   * @return The area of the location in square meters.
-   */
-  fun computeArea(): Double {
-    return TurfMeasurement.area(toPolygon())
-  }
-
-  /**
-   * Compute the height and width of the location in meters.
-   *
-   * @return A pair of the height and width of the location in meters.
-   */
-  fun computeHeightAndWidth(): Pair<Double, Double> {
-    if (topLeft == null || topRight == null || bottomLeft == null || bottomRight == null) {
-      Log.e("Location", "Cannot compute height and width of location, some corners are null")
-      return Pair(0.0, 0.0)
+    /**
+     * Compute the area of the location in square meters. The area is computed using the Turf
+     * Measurement library.
+     *
+     * @return The area of the location in square meters.
+     */
+    fun computeArea(): Double {
+        return TurfMeasurement.area(toPolygon())
     }
-    val height = TurfMeasurement.distance(topLeft, bottomLeft, "meters")
-    val width = TurfMeasurement.distance(topLeft, topRight, "meters")
-    return Pair(height, width)
-  }
 
-  constructor(
-      topLeft: Point,
-      topRight: Point,
-      bottomRight: Point,
-      bottomLeft: Point
-  ) : this(
-      Point.fromLngLat(
-          (topLeft.longitude() + bottomRight.longitude()) / 2,
-          (topLeft.latitude() + bottomRight.latitude()) / 2),
-      topLeft,
-      topRight,
-      bottomLeft,
-      bottomRight)
+    /**
+     * Compute the height and width of the location in meters.
+     *
+     * @return A pair of the height and width of the location in meters.
+     */
+    fun computeHeightAndWidth(): Pair<Double, Double> {
+        if (topLeft == null || topRight == null || bottomLeft == null || bottomRight == null) {
+            Log.e("Location", "Cannot compute height and width of location, some corners are null")
+            return Pair(0.0, 0.0)
+        }
+        val height = TurfMeasurement.distance(topLeft, bottomLeft, "meters")
+        val width = TurfMeasurement.distance(topLeft, topRight, "meters")
+        return Pair(height, width)
+    }
 
-  constructor(center: Point) : this(center, null, null, null, null)
+    constructor(
+        topLeft: Point,
+        topRight: Point,
+        bottomRight: Point,
+        bottomLeft: Point
+    ) : this(
+        Point.fromLngLat(
+            (topLeft.longitude() + bottomRight.longitude()) / 2,
+            (topLeft.latitude() + bottomRight.latitude()) / 2),
+        topLeft,
+        topRight,
+        bottomLeft,
+        bottomRight)
 
-  constructor(
-      listPoints: List<Point>
-  ) : this(listPoints[0], listPoints[1], listPoints[2], listPoints[3])
+    constructor(center: Point) : this(center, null, null, null, null)
+
+    constructor(
+        listPoints: List<Point>
+    ) : this(listPoints[0], listPoints[1], listPoints[2], listPoints[3])
 }
