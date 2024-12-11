ee0af5de1854d45d08bd74d714be384203db9a2f
AlexandreBeeDeeBop <alexandre.majchrzak@epfl.ch>
2024-12-11 18:12:55 +0100

    feat: implement new molecule in profile screen
    - Replace "@username" with "[symbol level] username".

--- app/src/main/java/com/github/se/cyrcle/ui/profile/DisplayProfileComponent.kt
+++ app/src/main/java/com/github/se/cyrcle/ui/profile/DisplayProfileComponent.kt
@@ -1,5 +1,7 @@
 package com.github.se.cyrcle.ui.profile
 
+import android.text.Html
+import android.widget.TextView
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,9 +14,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import com.github.se.cyrcle.R
 import com.github.se.cyrcle.model.user.User
+import com.github.se.cyrcle.model.user.UserLevelDisplay
 import com.github.se.cyrcle.ui.theme.atoms.Text
+import com.github.se.cyrcle.ui.theme.molecules.UsernameMolecule
 
 /**
  * Display the profile content of the user. This composable is a component meant to be used within
@@ -25,49 +30,57 @@ import com.github.se.cyrcle.ui.theme.atoms.Text
  */
 @Composable
 fun DisplayProfileComponent(user: User?, extras: @Composable () -> Unit) {
-  if (user == null) {
-    Text(stringResource(R.string.profile_is_null), Modifier.testTag("NullUserText"))
-    return
-  }
+    if (user == null) {
+        Text(stringResource(R.string.profile_is_null), Modifier.testTag("NullUserText"))
+        return
+    }
 
-  val firstName = user.details?.firstName ?: ""
-  val lastName = user.details?.lastName ?: ""
-  val username = user.public.username
-  val profilePictureUrl = user.localSession?.profilePictureUrl ?: ""
+    val firstName = user.details?.firstName ?: ""
+    val lastName = user.details?.lastName ?: ""
+    val username = user.public.username
+    val profilePictureUrl = user.localSession?.profilePictureUrl ?: ""
+    val reputationScore = user.public.userReputationScore
 
-  Column(
-      modifier = Modifier.fillMaxSize().testTag("ProfileContent"),
-      horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .testTag("ProfileContent"),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Column(
             modifier = Modifier.padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally) {
-              Text(
-                  text = firstName,
-                  style = MaterialTheme.typography.headlineMedium,
-                  testTag = "DisplayFirstName")
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = firstName,
+                style = MaterialTheme.typography.headlineMedium,
+                testTag = "DisplayFirstName"
+            )
 
-              Text(
-                  text = lastName,
-                  style = MaterialTheme.typography.headlineMedium,
-                  testTag = "DisplayLastName")
+            Text(
+                text = lastName,
+                style = MaterialTheme.typography.headlineMedium,
+                testTag = "DisplayLastName"
+            )
 
-              Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
-              ProfileImageComponent(
-                  url = profilePictureUrl,
-                  onClick = {},
-                  isEditable = false,
-                  modifier = Modifier.testTag("ProfileImage"))
+            ProfileImageComponent(
+                url = profilePictureUrl,
+                onClick = {},
+                isEditable = false,
+                modifier = Modifier.testTag("ProfileImage")
+            )
 
-              Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(8.dp))
 
-              Text(
-                  text = stringResource(R.string.view_profile_screen_display_username, username),
-                  style = MaterialTheme.typography.bodyMedium,
-                  testTag = "DisplayUsername")
+            UsernameMolecule(
+                username = username,
+                userReputationScore = reputationScore
+            )
 
-              Spacer(modifier = Modifier.height(16.dp))
-            }
+            Spacer(modifier = Modifier.height(16.dp))
+        }
         extras()
-      }
+    }
 }
