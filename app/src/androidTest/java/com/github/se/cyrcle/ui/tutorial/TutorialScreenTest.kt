50cc7add29f8b647a71421a12c1a2f034913f093
André Peiry <andre.peiry@epfl.ch>
2024-12-13 03:14:08 +0100

    feat: create Tutorial screen
    Need the images to fill the explanations.

---
+++ app/src/androidTest/java/com/github/se/cyrcle/ui/tutorial/TutorialScreenTest.kt
@@ -0,0 +1,84 @@
+package com.github.se.cyrcle.ui.tutorial
+
+import androidx.compose.ui.test.assertHasClickAction
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import com.github.se.cyrcle.ui.navigation.NavigationActions
+import com.github.se.cyrcle.ui.navigation.Route
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+
+class TutorialScreenTest {
+
+  private lateinit var navigationActions: NavigationActions
+
+  @get:Rule val composeTestRule = createComposeRule()
+
+  @Before
+  fun setup() {
+    navigationActions = mock(NavigationActions::class.java)
+  }
+
+  @Test
+  fun tutorialScreenBaseDisplayed() {
+    composeTestRule.setContent { TutorialScreen(navigationActions) }
+
+    composeTestRule.onNodeWithTag("TutorialScreen", true).assertIsDisplayed()
+
+    composeTestRule.onNodeWithTag("TutorialScreenTopPart", true).assertIsDisplayed()
+    composeTestRule.onNodeWithTag("TutorialScreenBottomPart", true).assertIsDisplayed()
+
+    composeTestRule.onNodeWithTag("TutorialScreenNextButton", true).assertIsDisplayed()
+    composeTestRule.onNodeWithTag("TutorialScreenNextButtonText", true).assertIsDisplayed()
+    composeTestRule.onNodeWithTag("TutorialScreenSkipButton", true).assertIsDisplayed()
+  }
+
+  @Test
+  fun tutorialScreenTestBehavior() {
+    composeTestRule.setContent { TutorialScreen(navigationActions) }
+
+    fun nextPage() {
+      composeTestRule
+          .onNodeWithTag("TutorialScreenNextButton", true)
+          .assertIsDisplayed()
+          .assertHasClickAction()
+          .performClick()
+    }
+
+    // Welcome Screen
+    composeTestRule.onNodeWithTag("TutorialScreenWelcomeTitle", true).assertIsDisplayed()
+    composeTestRule.onNodeWithTag("TutorialScreenWelcomeSubtitle", true).assertIsDisplayed()
+    nextPage()
+
+    // Images
+    composeTestRule.onNodeWithTag("TutorialScreenImage1", true).assertIsDisplayed()
+    nextPage()
+    composeTestRule.onNodeWithTag("TutorialScreenImage2", true).assertIsDisplayed()
+    nextPage()
+
+    // Thank you Screen
+    composeTestRule.onNodeWithTag("TutorialScreenThankYouTitle", true).assertIsDisplayed()
+    composeTestRule.onNodeWithTag("TutorialScreenThankYouSubtitle", true).assertIsDisplayed()
+    nextPage()
+
+    verify(navigationActions).navigateTo(Route.MAP)
+  }
+
+  @Test
+  fun tutorialScreenSkipNavigateToMap() {
+    composeTestRule.setContent { TutorialScreen(navigationActions) }
+
+    composeTestRule
+        .onNodeWithTag("TutorialScreenSkipButton")
+        .assertIsDisplayed()
+        // .assertHasClickAction() // Can not be found on a clickable text
+        .performClick()
+
+    verify(navigationActions).navigateTo(Route.MAP)
+  }
+}
