3ccb632bc2a24420b85f613acee9b5186e600f06
André Peiry <andre.peiry@epfl.ch>
2024-12-13 05:25:09 +0100

    feat: add real tutorial screen

--- app/src/androidTest/java/com/github/se/cyrcle/ui/tutorial/TutorialScreenTest.kt
+++ app/src/androidTest/java/com/github/se/cyrcle/ui/tutorial/TutorialScreenTest.kt
@@ -1,12 +1,15 @@
 package com.github.se.cyrcle.ui.tutorial
 
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.tooling.preview.Preview
 import com.github.se.cyrcle.ui.navigation.NavigationActions
 import com.github.se.cyrcle.ui.navigation.Route
+import com.github.se.cyrcle.ui.theme.CyrcleTheme
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -56,9 +59,9 @@ class TutorialScreenTest {
     nextPage()
 
     // Images
-    composeTestRule.onNodeWithTag("TutorialScreenImage1", true).assertIsDisplayed()
+    composeTestRule.onNodeWithTag("TutorialScreenImageMap", true).assertIsDisplayed()
     nextPage()
-    composeTestRule.onNodeWithTag("TutorialScreenImage2", true).assertIsDisplayed()
+    composeTestRule.onNodeWithTag("TutorialScreenImageList", true).assertIsDisplayed()
     nextPage()
 
     // Thank you Screen
@@ -82,3 +85,4 @@ class TutorialScreenTest {
     verify(navigationActions).navigateTo(Route.MAP)
   }
 }
+
