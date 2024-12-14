48c9ef0fee5fc1a29fc9279e85c7d91baa01d746
Maria Bennani <maria.bennani@epfl.ch>
2024-12-14 14:05:25 +0100

    feat: add autoresize text composable and integrate it in topBar

--- app/src/main/java/com/github/se/cyrcle/ui/theme/molecules/TopAppBar.kt
+++ app/src/main/java/com/github/se/cyrcle/ui/theme/molecules/TopAppBar.kt
@@ -15,16 +15,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.github.se.cyrcle.ui.navigation.NavigationActions
-import com.github.se.cyrcle.ui.theme.Typography
 
 /**
  * Create a themed bottom navigation bar, with simplified arguments.
@@ -38,18 +42,15 @@ import com.github.se.cyrcle.ui.theme.Typography
 fun TopAppBar(navigationActions: NavigationActions, title: String, testTag: String = "TopAppBar") {
   val configuration = LocalConfiguration.current
   val screenWidth = configuration.screenWidthDp
-  val scaledFontSize = (screenWidth * 0.05).sp // Adjust the scaling factor as needed
+  val initialFontSize = (screenWidth * 0.05).sp
 
   CenterAlignedTopAppBar(
       title = {
-        Text(
+        AutoResizingText(
             text = title,
-            style =
-                Typography.titleLarge.copy(fontSize = scaledFontSize, fontWeight = FontWeight.Bold),
-            color = Color.White,
-            modifier = Modifier.testTag("${testTag}Title"),
-            maxLines = 2,
-            overflow = TextOverflow.Ellipsis)
+            initialFontSize = initialFontSize,
+            modifier = Modifier.testTag("${testTag}Title").padding(horizontal = 8.dp),
+            maxLines = 2)
       },
       colors =
           TopAppBarColors(
@@ -72,6 +73,45 @@ fun TopAppBar(navigationActions: NavigationActions, title: String, testTag: Stri
                     contentDescription = "Go Back",
                     modifier = Modifier.size(24.dp))
               }
+        }
+      })
+}
+
+/**
+ * A composable function that manages the resizing of the text to fit the available space. The text
+ * will be resized until it fits the available space, or until the font size is 6sp. Both the font
+ * size and the line height will be reduced by 5% at each iteration.
+ *
+ * @param text The text to display
+ * @param initialFontSize The initial font size of the text
+ * @param modifier The modifier of the text
+ * @param maxLines The maximum number of lines the text can take
+ */
+@Composable
+fun AutoResizingText(
+    text: String,
+    initialFontSize: TextUnit,
+    modifier: Modifier = Modifier,
+    maxLines: Int
+) {
+  var fontSize by remember { mutableStateOf(initialFontSize) }
+  var lineHeight by remember { mutableStateOf(initialFontSize * 1.2f) }
+  var readyToDraw by remember { mutableStateOf(false) }
+
+  Text(
+      text = text,
+      color = Color.White,
+      fontSize = fontSize,
+      fontWeight = FontWeight.Bold,
+      lineHeight = lineHeight,
+      maxLines = maxLines,
+      modifier = modifier.drawWithContent { if (readyToDraw) drawContent() },
+      onTextLayout = { textLayoutResult ->
+        if (textLayoutResult.didOverflowHeight && fontSize > 6.sp) {
+          fontSize *= 0.95f
+          lineHeight *= 0.95f
+        } else {
+          readyToDraw = true
         }
       })
 }
