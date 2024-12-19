806e8848eaf47982ec2f3e8d9b9b2a130fd79679
Maria Bennani <maria.bennani@epfl.ch>
2024-12-19 17:55:32 +0100

    refactor: remove todos

--- app/src/androidTest/java/com/github/se/cyrcle/ui/theme/molecules/EnumDropDownTest.kt
+++ app/src/androidTest/java/com/github/se/cyrcle/ui/theme/molecules/EnumDropDownTest.kt
@@ -33,18 +33,12 @@ class EnumDropDownTest {
     composeTestRule.onNodeWithTag("${tagD}Label", true).assertIsDisplayed()
     composeTestRule.onNodeWithTag(tagD).performClick()
     parkingList.withIndex().forEach { (_, _) ->
-      // TODO For some reason, the menu items are not checked
-      // composeTestRule.onNodeWithTag("${tagD}${i}Item", true).assertIsDisplayed()
-      // composeTestRule.onNodeWithTag("${tagD}${i}Text", true).assertIsDisplayed()
     }
 
     composeTestRule.onNodeWithTag(tag1, true).assertIsDisplayed()
     composeTestRule.onNodeWithTag("${tag1}Label", true).assertIsDisplayed()
     composeTestRule.onNodeWithTag(tag1, true).performClick()
     parkingList.withIndex().forEach { (_, _) ->
-      // TODO For some reason, the menu items are not checked
-      // composeTestRule.onNodeWithTag("${tag1}${i}Item", true).assertIsDisplayed()
-      // composeTestRule.onNodeWithTag("${tag1}${i}Text", true).assertIsDisplayed()
     }
   }
 
