5581a2213f83875d80a2f41efb73415766fffe22
Maria Bennani <maria.bennani@epfl.ch>
2024-12-19 18:15:46 +0100

    refactor: remove useless use of val

--- app/src/main/java/com/github/se/cyrcle/model/image/ImageRepositoryCloudStorage.kt
+++ app/src/main/java/com/github/se/cyrcle/model/image/ImageRepositoryCloudStorage.kt
@@ -16,7 +16,7 @@ const val MAX_IMAGE_SIZE = 200
 
 class ImageRepositoryCloudStorage
 @Inject
-constructor(private val auth: FirebaseAuth, private val storage: FirebaseStorage) :
+constructor(private val auth: FirebaseAuth, storage: FirebaseStorage) :
     ImageRepository {
 
   private val storageRef: StorageReference = storage.reference
